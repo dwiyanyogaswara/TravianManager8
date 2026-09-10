eeb8a4f9c93cca1ffaa21ba8b419d322844180a5
Dwiyan Yogaswara <dwiyanyogaswara@gmail.com>
2026-09-10 09:48:40 +0700

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.travianfarmassistant"
         minSdk = 26
         targetSdk = 35
-        versionCode = 37
-        versionName = "4.14.6"
+        versionCode = 38
+        versionName = "4.14.7"
     }
 
     compileOptions {
